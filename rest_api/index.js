235922420cec3d11c479e59e494205946e860a58
Dan Stoian <danmihaistoian@gmail.com>
2022-01-27 21:56:46 +0200

    fixed listening port on hapi and mongo connection

--- rest_api/index.js
+++ rest_api/index.js
@@ -22,7 +22,7 @@ const init = async () => {
 
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost'
+        host: '0.0.0.0'
     });
 
     server.route({
@@ -30,7 +30,7 @@ const init = async () => {
         path: '/',
         handler: async (request, h) => {
 
-
+            return 'Hello World!';
         }
     });
 
