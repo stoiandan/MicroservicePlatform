8f5ce9068d3860a8fc234d2af8576387905f7e55
Dan Stoian <danmihaistoian@gmail.com>
2022-01-29 15:11:04 +0200

    Added button to generate data randomly; fixed bug that recreated component

--- login/src/components/dataPage.js
+++ login/src/components/dataPage.js
@@ -6,24 +6,34 @@ import { useEffect, useState } from "react";
 export default function DataPage(props) {
     const [data, setData] = useState({name: "no name", age: 0});
     useEffect(() =>{
-        async function getPostData() {
-            const response = await fetch('http://localhost:1945', {
-                method: 'POST',
-                headers: { 'Accept': 'application/json' }
-            });
-            const obj = await response.body.getReader().read();
-            const str = new TextDecoder().decode(obj.value);
-            setData(JSON.parse(str));
-        }
-         getPostData();
+        (async () => {
+            setData(await getPersonAsync());
+        })();
     }, []);
 
     return (
         <div>
+            <nav>
+                <ul>
+                    <li>
+                        <input type="button" value="Generate Random data" onClick={async () => setData( await getPersonAsync())}/>
+                    </li>
+                </ul>
+            </nav>
             <h1>Welcome to the data presented</h1>
             <h2>My name is {data.name} and my age is {data.age}</h2>
         </div>
     );
+}
+
+async function getPersonAsync() {
+    const response = await fetch('http://localhost:1945', {
+        method: 'POST',
+        headers: { 'Accept': 'application/json' }
+    });
+    const obj = await response.body.getReader().read();
+    const str = new TextDecoder().decode(obj.value);
+    return JSON.parse(str);
 }
 
 
